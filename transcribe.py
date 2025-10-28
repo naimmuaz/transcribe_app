b5621c70d02bd4361b4b7097fdf004334bb55e8a
Naim Muaz <naimmuaz@Naims-MacBook-Air.local>
2025-10-28 22:55:38 +1000

    testing whisper and modified .gitignore

--- transcribe.py
+++ transcribe.py
@@ -1,3 +1,7 @@
 import whisper
 
+file = "'/Users/naimmuaz/Downloads/STOP WASTING YEARS. 30 DAYS IS ALL IT TAKES.mp3'"
+
 model = whisper.load_model('turbo')
+result = model.transcribe(file)
+print(result)
