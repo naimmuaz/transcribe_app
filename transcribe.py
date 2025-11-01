0b4d676120be5776e4d7b0040de9fb9c6374fbde
Naim Muaz <naimmuaz@Naims-MacBook-Air.local>
2025-11-01 20:01:25 +1000

    modified .gitignore

--- transcribe.py
+++ transcribe.py
@@ -5,6 +5,5 @@ file = '/Users/naimmuaz/Downloads/test.mp3'
 model = whisper.load_model("turbo")
 result = model.transcribe(file, fp16=False)
 
-
 # with open("transcribe_file.txt", "w") as f:
 #   f.write(result["text"])
